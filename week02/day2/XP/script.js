1f2538e24627d0f136bf9b23b2b30fb4bb7c9569
Yishac Yosef Yona <yishacyosef@gmail.com>
2021-12-13 17:07:35 +0200

    commit2

--- week02/day2/XP/script.js
+++ week02/day2/XP/script.js
@@ -1,11 +1,44 @@
-let x = 10;
-let y = 20;
+//EXERCISE 1//
 
-if(x > y){
-    console.log(y );
+let number1 = 10;
+let number2 = 20;
+
+if(number1 > number2){
+    console.log(`${number1} is bigger than ${number2}`);
 } 
 else {
-    console.log(x);
+    console.log(`${number2} is bigger than ${number1}`);
 }
 
- 
+//EXERCISE 2// 
+let newDog = "Chihuahua";
+console.log(newDog.length);
+console.log(newDog.toUpperCase());
+console.log(newDog.toLowerCase());
+if (newDog == "Chihuahua"){
+    console.log(`I love Chihuahuas, it’s my favorite dog breed`);  
+}else{
+    console.log(`I dont care, I prefer cats`);
+}
+
+//EXERCISE 3//
+
+let numberOfUser = prompt(`Hi, please enter a number, we will check if it is even or odd`);
+let evenoo = (numberOfUser%2);
+if (evenoo == 0) {
+console.log(`${numberOfUser} is an even number`);
+} else{
+    console.log(`${numberOfUser} os an odd number`);
+}
+
+//EXERCISE 4//
+let users = ["Lea123", "Princess45", "cat&doglovers", "helooo@000"];
+if (users.length ==0){
+    console.log(`no one is online`);
+} else if (users.length ==1){
+    console.log(users)
+} else if (users.length ==2){
+    console.log(`${users[0]} and ${users[1]} are online`)
+} else{
+    console.log(`${users[0]} and ${users[1]} and ${users.length-2} more are online`);
+}
