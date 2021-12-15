843b4b695eea0e078391dd5fd2c0ddb55da907e3
Yishac Yosef Yona <yishacyosef@gmail.com>
2021-12-15 12:29:56 +0200

    firstcommit

--- week02/day1/XP/script.js
+++ week02/day1/XP/script.js
@@ -29,3 +29,159 @@ console.log(a+b) //second expression
 // Prediction:23
 // Actual:23
 
+// Exercise 5 : Guess The Answers #2
+// Instructions
+// For each expression, predict what you think the output will be in a comment (//) without first running the command.
+// Of course, explain each prediction.
+// Then run the expression in the console. Note the actual output in a comment and compare it with your prediction.
+// For Example
+// typeof("potato")
+// // Prediction: Vegetable
+// // Actual: String
+// What is the output of each of the expressions below?
+// typeof(15)
+// // Prediction: Number
+// // Actual: Number
+console.log(typeof(15))
+// typeof(5.5)
+// // Prediction: Number 
+// // Actual:Number
+console.log(typeof(5.5))
+
+// typeof(NaN)
+// // Prediction: number
+// // Actual:number
+console.log(typeof(NaN))
+
+// typeof("hello")
+// // Prediction: string
+// // Actual: string
+console.log(typeof("hello"))
+
+// typeof(true)
+// // Prediction: boolean
+// // Actual: boolean
+console.log(typeof(true))
+
+
+// typeof(1 != 2)
+// // Prediction: boolean
+// // Actual: boolean
+console.log(typeof(1 != 2))
+
+// "hamburger" + "s"
+// // Prediction: NaN
+// // Actual: hamburgers
+console.log("hamburger" + "s")
+
+// "hamburgers" - "s"
+// // Prediction:NaN
+// // Actual:NaN
+console.log("hamburger" - "s")
+
+// "1" + "3"
+// // Prediction: 13
+// // Actual:
+console.log("1" + "3")
+
+// "1" - "3"
+// // Prediction: -2    
+// // Actual: -2
+console.log("1" - "3")
+
+// "johnny" + 5
+// // Prediction:
+// // Actual:johnny5
+console.log("johnny" + 5)
+// "johnny" - 5
+// // Prediction: johnny5
+// // Actual: johnny5
+console.log("johnny" - 5)
+
+// 99 * "hello"
+// // Prediction:NaN
+// // Actual:NaN
+console.log(99 * "hello")
+
+// 1 != 1
+// // Prediction: Fals
+// // Actual: False
+console.log(1 != 1)
+
+// 1 == "1"
+// // Prediction: True
+// // Actual:True 
+console.log(1 == "1")
+
+// 1 === "1"
+// // Prediction: False
+// // Actual:False
+console.log(1 === "1")
+
+// Exercise 6 : Guess The Answers #3
+// Instructions
+// For each expression, predict what you think the output will be in a comment (//) without first running the command.
+// Of course, explain each prediction.
+// Then run the expression in the console. Note the actual output in a comment and compare it with your prediction.
+
+
+
+// What is the output of each of the expressions below?
+
+
+// 5 + "34"
+// // Prediction: 534
+// // Actual:534
+console.log(5 + "34")
+// 5 - "4"
+// // Prediction:1
+// // Actual:1
+console.log(5 - "4")
+// 10 % 5
+// // Prediction:0
+// // Actual:0
+console.log(10 % 5)
+// 5 % 10
+// // Prediction:5
+// // Actual:5
+console.log(5 % 10)
+// "Java" + "Script"
+// // Prediction: JavaScript
+// // Actual: JavaScript
+console.log("Java" + "Script")
+// " " + " "
+// // Prediction: ?
+// // Actual:
+console.log(" " + " ")
+// " " + 0
+// // Prediction: 0
+// // Actual: 0
+console.log(" " + 0)
+// true + true
+// // Prediction: ?
+// // Actual: 2
+console.log(true + true)
+// true + false
+// // Prediction: ?
+// // Actual:1
+console.log(true + false)
+// false + true
+// // Prediction: ?
+// // Actual:1
+console.log(false + true)
+// false - true
+// // Prediction:NaN
+// // Actual:-1
+console.log(false - true)
+// !true
+// // Prediction:?
+// // Actual:false
+console.log(!true)
+// 3 - 4
+// // Prediction:-1
+// // Actual:-1
+console.log(3 - 4)
+// "Bob" - "bill"
+// // Prediction:NaN
+// // Actual:NaN
+console.log("Bob" - "bill")
