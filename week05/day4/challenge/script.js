4706020f97eb3becef587c2a60ccba6c480ce3de
Yishac Yosef Yona <yishacyosef@gmail.com>
2022-01-05 13:19:08 +0200

    mittagessen

--- week05/day4/challenge/script.js
+++ week05/day4/challenge/script.js
@@ -12,3 +12,33 @@ async function getData (){
     let object1 = await xp.json()
     console.log(object1)
 }
+
+getData()
+
+/*Exercise 2: Analyze
+Instructions
+
+//This function with setTimeOut is going to be called in the 
+//next async func.
+
+function resolveAfter2Seconds() {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve('resolved');
+        }, 2000);
+    });
+}
+
+async function asyncCall() {
+    console.log('calling');
+    let result = await resolveAfter2Seconds();
+    console.log(result);
+}
+
+asyncCall();
+Analyze the code provided above what will be the outcome?
+
+
+//it will console.log "calling", then call the function with 
+setTimeOut, if everything went right it will console.log result
+*/
