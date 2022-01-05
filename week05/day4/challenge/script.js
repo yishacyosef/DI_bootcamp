f4f5531aba71f37be94c46e5a896a0b181ea4471
Yishac Yosef Yona <yishacyosef@gmail.com>
2022-01-05 21:52:17 +0200

    gutenacht

---
+++ week05/day4/challenge/script.js
@@ -0,0 +1,17 @@
+/*instructions
+Implement a simple version of Promise.all.
+This function should accept an array of promises and return an array of resolved values.
+If any of the promises are rejected, the function should catch them.
+
+Example
+*/
+const promise1 = Promise.resolve(3);
+const promise2 = 42;
+const promise3 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 3000, 'foo');
+});
+// expected output: Array [3, 42, "foo"]
+
+Promise.all([promise1, promise2, promise3]).then(values =>{
+    console.log(values); 
+});
