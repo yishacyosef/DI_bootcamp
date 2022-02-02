fdf867d0027376953872b971959f775a974323d0
Yishac Yosef Yona <yishacyosef@gmail.com>
2022-02-02 12:41:15 +0200

    hello

--- week09/day03/andrei/src/containers/App.js
+++ week09/day03/andrei/src/containers/App.js
@@ -1,27 +1,57 @@
 import React, {component } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import { setSearchField } from '../actions';
-
-
 import './App.css';
+
+const mapStateToProps = state =>{
+  return{
+    searchField: state.searchfield
+  }
+}
+const mapDispatchToProps = (dispatch) => {
+ return {
+   onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+ }
+}
 
 class App extends Component{
   constructor() {
     super() 
       this.state = {
-        robots: [],
-        searchfield: ''
+        robots: []
       }
   }
 
   componentDidMount(){
       fetch('https://jsonplaceholder.typicode.com/useres')
       .then(response => response.json())
-      .then(useres => {this.setState({ robots: users})})
+      .then(useres => {this.setState({ robots: users})});
   }
-} 
 
 
-export default App;
+render() {
+  const {robots} = this.state;
+  const { searchField, onSearchChange } = this.props;
+  const filteredRobots = robots.filter(robot =>{
+    return robot.name.toLowerCase().includes(searchField.toLowerCase());
+  })
+  return !robots.lenght ?
+    <h1>Loading</h1> :
+    (
+    <div className = "tc">
+      <h1 className='f1'>RoboFriends</h1>
+      <searchBox searchChange = {onSearchChange}/>
+      <Scroll>
+        <CardList robots= {filteredRobots}/>
+      </Scroll>
+    </div>
+  );
+}
+}
+ 
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
