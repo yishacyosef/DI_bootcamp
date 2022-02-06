255c71d1e6a33b588279f002a056d332f90d57eb
Yishac Yosef Yona <yishacyosef@gmail.com>
2022-02-06 14:09:03 +0200

    gutewoche

--- week09/day03/andrei/src/containers/App.js
+++ week09/day03/andrei/src/containers/App.js
@@ -23,7 +23,7 @@ class App extends Component{
       this.state = {
         robots: []
       }
-  }
+    }
 
   componentDidMount(){
       fetch('https://jsonplaceholder.typicode.com/useres')
@@ -32,26 +32,25 @@ class App extends Component{
   }
 
 
-render() {
-  const {robots} = this.state;
-  const { searchField, onSearchChange } = this.props;
-  const filteredRobots = robots.filter(robot =>{
-    return robot.name.toLowerCase().includes(searchField.toLowerCase());
-  })
-  return !robots.lenght ?
-    <h1>Loading</h1> :
-    (
-    <div className = "tc">
-      <h1 className='f1'>RoboFriends</h1>
-      <searchBox searchChange = {onSearchChange}/>
-      <Scroll>
-        <CardList robots= {filteredRobots}/>
-      </Scroll>
-    </div>
-  );
-}
+  render() {
+    const {robots} = this.state;
+    const { searchField, onSearchChange } = this.props;
+    const filteredRobots = robots.filter(robot =>{
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
+    })
+    return !robots.lenght ?
+      <h1>Loading</h1> :
+      (
+      <div className = "tc">
+        <h1 className='f1'>RoboFriends</h1>
+        <searchBox searchChange = {onSearchChange}/>
+        <Scroll>
+          <CardList robots= {filteredRobots}/>
+        </Scroll>
+      </div>
+    );
+  }
 }
  
-
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
