cdc8c8fac00474d911916dd2b0b4ae6783b7dc11
Yishac Yosef Yona <yishacyosef@gmail.com>
2022-02-07 13:47:31 +0200

    footbook

--- finalProjects/feetbook/src/components/Header.jsx
+++ finalProjects/feetbook/src/components/Header.jsx
@@ -1,16 +1,43 @@
 import React from 'react';
 import '../css/Header.css';
 import SearchIcon from '@mui/icons-material/Search';
+import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
+import HomeIcon from '@mui/icons-material/Home';
+import FlagIcon from '@mui/icons-material/Flag';
+import SubscriptionsOutlinedIcon from '@mui/icons-material/SubscriptionsOutlined';
+import StorefrontOutlinedIcon from '@mui/icons-material/StorefrontOutlined';
+import SupervisedUserCircleIcon from '@mui/icons-material/SupervisedUserCircle';
+import Avatar from '@mui/material/Avatar';
 
 
 function Header() {
   return<div className='header'>
       <div className="header__left"></div>
-        <img src="../icons/icons8-facebook.svg" alt="logo"/>
+        <FacebookRoundedIcon color ="primary" sx={{fontSize: 40}}/>
         <div className="header__input"></div>
         <SearchIcon/>
+        <input type="text" />
       <div className="header__middle"></div>
-      <div className="header__right"></div>
+        <div className="header__option"></div>
+            <HomeIcon fontSize="large"/>
+        <div className="header__option"></div>
+            <FlagIcon fontSize="large"/>
+        <div className="header__option"></div>
+            <SubscriptionsOutlinedIcon fontSize="large"/>
+        <div className="header__option"></div>
+            <StorefrontOutlinedIcon fontSize="large"/>
+        <div className="header__option"></div>
+            <SupervisedUserCircleIcon fontSize="large"/>  
+
+      <div className="header__right">
+          <div className="header__info"></div>
+            <Avatar/>
+            <h4>yishac</h4>
+      </div>
+
+      <IconButton>
+          <AddIcon/>
+      </IconButton>
   </div>
 }
 
