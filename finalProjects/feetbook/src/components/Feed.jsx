98225552dfbce7c052a18d3e19d71951c582081a
Yishac Yosef Yona <yishacyosef@gmail.com>
2022-02-11 15:21:55 +0200

    story

---
+++ finalProjects/feetbook/src/components/Feed.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../css/Feed.css"
+
+function Feed() {
+  return (
+    <div className='feed'>
+    {/*StoryReel*/}
+    {/* MessageSender*/}
+    </div>
+  )
+}
+
+export default Feed
