796f88087e7f6f11ba01d8ec91c4ffa133cfdf47
Yishac Yosef Yona <yishacyosef@gmail.com>
2022-02-12 21:43:29 +0200

    Story reel fixed!!!

--- finalProjects/feetbook/src/components/Story.jsx
+++ finalProjects/feetbook/src/components/Story.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../css/Story.css';
 import { Avatar } from '@mui/material';
+
+
 function Story({ image, profileSrc, title}) {
   return (
     <div style ={{ backgroundImage: `url(${image})` }}
     className='story'>
-      <Avatar src={profileSrc} />
+      <Avatar className = "story__avatar" src={profileSrc} />
       <h4>{title}</h4>
     </div>
   )
