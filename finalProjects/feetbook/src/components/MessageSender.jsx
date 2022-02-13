3bc073a50860f08276705efb55f6bf0a0aa0528a
Yishac Yosef Yona <yishacyosef@gmail.com>
2022-02-13 13:13:43 +0200

    adding messageSender

--- finalProjects/feetbook/src/components/MessageSender.jsx
+++ finalProjects/feetbook/src/components/MessageSender.jsx
@@ -1,6 +1,10 @@
 import { Avatar } from '@mui/material'
 import React from 'react'
 import '../css/MessageSender.css'
+import VideocamIcon from '@mui/icons-material/Videocam';
+import PhotoLibraryIcon from '@mui/icons-material/PhotoLibrary';
+import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
+
 
 function MessageSender() {
     const handleSubmit = (e) => {
@@ -22,6 +26,18 @@ function MessageSender() {
             </form>
         </div>
         <div className="messageSender__bottom">
+            <div className="messageSender__option">
+                <VideocamIcon style={{ color: "red"}} />
+                <h3>Live Video</h3>
+            </div>
+            <div className="messageSender__option">
+                <PhotoLibraryIcon style={{color: "green"}} />
+                <h3>Photo/Video</h3>
+            </div>
+            <div className="messageSender__option">
+                <InsertEmoticonIcon style={{color: "orange"}} />
+                <h3>Feeling/Activity</h3>
+            </div>
 
         </div>
     </div>
