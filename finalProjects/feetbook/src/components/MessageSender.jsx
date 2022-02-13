722f18187efc66c8111c1e713b1ec7a4bd51b21d
Yishac Yosef Yona <yishacyosef@gmail.com>
2022-02-13 12:02:41 +0200

    form creation

--- finalProjects/feetbook/src/components/MessageSender.jsx
+++ finalProjects/feetbook/src/components/MessageSender.jsx
@@ -1,10 +1,31 @@
+import { Avatar } from '@mui/material'
 import React from 'react'
 import '../css/MessageSender.css'
 
 function MessageSender() {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
   return (
-    <div className='messageSender'></div>
+    <div className='messageSender'>
+        <div className='messageSender__top'>
+            <Avatar />
+            <form>
+                <input type="text" 
+                    className='messageSender__input'
+                    placeholder={`What's on your mind`} />
+                <input type="text"
+                    placeholder='Image URL (Optional)' />
+                <button onClick={handleSubmit} type = 'submit'>
+                    Hidden Submit
+                </button>
+            </form>
+        </div>
+        <div className="messageSender__bottom">
+
+        </div>
+    </div>
   )
-}
+}   
 
 export default MessageSender
