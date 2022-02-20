c8c343af4fcecd855d548d22cc0e46df289b3e69
Yishac Yosef Yona <yishacyosef@gmail.com>
2022-02-20 10:44:09 +0200

    facebook logo

---
+++ finalProjects/feetbook/src/components/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../css/Login.css'
+
+function login() {
+  return (
+    <div className='login'>login
+        <div className="login__logo">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/900px-Facebook_f_logo_%282019%29.svg.png?20200820101156" alt="Facebook logo" />
+            <img src="https://www.logo.wine/a/logo/Facebook/Facebook-Logo.wine.svg" alt="Facebook logo" />
+        </div>
+    </div>
+  )
+}
+
+export default login
