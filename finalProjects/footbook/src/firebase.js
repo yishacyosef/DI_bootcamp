98a03b027d23cb9c68af533a9e5cc0a2d30445a5
Yishac Yosef Yona <yishacyosef@gmail.com>
2022-02-27 11:40:41 +0200

    firebase

--- finalProjects/footbook/src/firebase.js
+++ finalProjects/footbook/src/firebase.js
@@ -1,4 +1,9 @@
+import firebase from 'firebase'
+;
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyDpH-mlBI2M4_Iia8lff8h3Hgu-bDeNrW0",
     authDomain: "footbook-75b38.firebaseapp.com",
@@ -8,3 +13,6 @@ const firebaseConfig = {
     appId: "1:900835082116:web:b75a3e89bb3bdf448be7d1",
     measurementId: "G-YH6EE5NZQM"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
